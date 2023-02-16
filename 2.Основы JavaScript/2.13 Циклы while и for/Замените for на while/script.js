130d7afaa0336e735a8220c081ea1263f747e6b2
Vitaly <poroshenkoe@gmail.com>
2023-02-17 02:23:49 +0300

    add: 2.13

---
+++ 2.Основы JavaScript/2.13 Циклы while и for/Замените for на while/script.js
@@ -0,0 +1,11 @@
+// Перепишите код, заменив цикл for на while, без изменения поведения цикла.
+
+for (let i = 0; i < 3; i++) {
+  alert(`number ${i}!`);
+}
+
+let a = 0;
+while (a < 3) {
+  console.log(`number ${a}!`);
+  a++;
+}
