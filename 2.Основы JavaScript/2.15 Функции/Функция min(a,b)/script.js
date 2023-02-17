a2d76fb63a115bb18dbb0136babb7be48beecd31
Vitaly <poroshenkoe@gmail.com>
2023-02-17 14:51:48 +0300

    add: 2.14-2.17

---
+++ 2.Основы JavaScript/2.15 Функции/Функция min(a,b)/script.js
@@ -0,0 +1,11 @@
+// Напишите функцию min(a,b), которая возвращает меньшее из чисел a и b.
+
+// Пример вызовов:
+
+function min(a, b) {
+  return a > b ? b : a;
+}
+
+console.log(min(2, 5));
+console.log(min(3, -1));
+console.log(min(1, 1));
